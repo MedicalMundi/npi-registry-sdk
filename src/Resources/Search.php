87ac1295e2fb79cfc5e5bb09e3902c45aed9acf4
zero <teclaizerai@gmail.com>
2020-11-17 12:41:39 +0100

    Make Search class as final.

--- src/Resources/Search.php
+++ src/Resources/Search.php
@@ -1,12 +1,10 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace MedicalMundi\NpiRegistry\SDK\Resources;
 
 use JustSteveKing\PhpSdk\Resources\AbstractResource;
 
-class Search extends AbstractResource
+final class Search extends AbstractResource
 {
     protected string $path = '/api';
 
--- tests/Resources/SearchTest.php
+++ tests/Resources/SearchTest.php
@@ -1,6 +1,5 @@
 <?php declare(strict_types=1);
 
-
 namespace MedicalMundi\NpiRegistry\SDK\Tests\Resources;
 
 use JustSteveKing\HttpSlim\HttpClient;
