78f9e39c7a84682e2d33f2b976ce2c0550141e90
pMononoke <francescabonadonna@gmail.com>
2020-11-29 14:34:07 +0100

    Fix doheader.

--- src/NpiRegistry.php
+++ src/NpiRegistry.php
@@ -1,5 +1,19 @@
 <?php declare(strict_types=1);
 
+/**
+ * This file is part of the medicalmundi/npi-registry-php-sdk library
+ *
+ * @copyright (c) Zerai Teclai <teclaizerai@googlemail.com>.
+ * @copyright (c) Francesca Bonadonna <francescabonadonna@googlemail.com>.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/MedicalMundi/npi-registry-sdk/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
 namespace MedicalMundi\NpiRegistry\SDK;
 
 use DI\Container;
